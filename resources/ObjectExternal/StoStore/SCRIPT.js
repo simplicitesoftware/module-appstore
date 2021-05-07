4df5b755451b0d69c5f72d527b9665b6715b0e93
designer (Designer) <no-reply@simplicite.fr>
2021-05-07 22:43:47 +0200

    Indent

--- resources/ObjectExternal/StoStore/SCRIPT.js
+++ resources/ObjectExternal/StoStore/SCRIPT.js
@@ -1,11 +1,11 @@
 var Store = (function() {
 	var data;
-	function fire(src){
+	function fire(src) {
 		data = prepareData(src);
 		render(0);
 	}
 	
-	function render(tab_index){
+	function render(tab_index) {
 		var r = $(Mustache.render($('#tmplStore').html(), data));
 		// tab initialization
 		r.find('.tabitem').hide();
@@ -14,16 +14,16 @@ var Store = (function() {
 		r.find('.nav li').click(tab);
 		
 		r.find('.install-module').click(callInstall);
-		r.find('.goto-module').click(function(){
+		r.find('.goto-module').click(function() {
 			$ui.clickReference(null, "Module", $(this).data('module-id'));
 		});
-		r.find('.goto-delete').click(function(){
+		r.find('.goto-delete').click(function() {
 			$ui.loadURL(null, data.delete_url+$(this).data('module-id'));
 		});
 		$('#store').html(r);
 	}
 
-	function tab(){
+	function tab() {
 		$('#store .nav li').removeClass('active');
 		$(this).addClass('active');
 		
@@ -31,9 +31,9 @@ var Store = (function() {
 		$('#tab'+$(this).data('tab')).show();
 	}
 	
-	function prepareData(src){
-		src.stores.map(function(store){
-			store.apps.map(function(app){
+	function prepareData(src) {
+		src.stores.map(function(store) {
+			store.apps.map(function(app) {
 				app.module_settings = JSON.stringify(app.module_settings, null, 4);
 				return app;
 			});
@@ -43,27 +43,26 @@ var Store = (function() {
 		return src;
 	}
 	
-	function callInstall(){
+	function callInstall() {
 		$ui.view.showLoading();
 		var module = $(this).data('module');
 		var storeIdx = $(this).data('store-idx');
 		$.ajax({
-	        'url' : data.install_url+"?install="+module+"&storeidx="+storeIdx,
-	        'type' : 'GET',
-	        'success' : function(updated_data) {
-	            data = prepareData(updated_data);
-	            render(storeIdx);
-	            $ui.view.hideLoading();
-	            optionalClearCache();
-	        },
-	        'error' : function(request,error)
-	        {
-	            alert("Request: "+JSON.stringify(request));
-	        }
-	    });
+			'url': data.install_url+"?install="+module+"&storeidx="+storeIdx,
+			'type': 'GET',
+			'success': function(updated_data) {
+				data = prepareData(updated_data);
+				render(storeIdx);
+				$ui.view.hideLoading();
+				optionalClearCache();
+			},
+			'error': function(request,error) {
+				alert("Request: "+JSON.stringify(request));
+			}
+		});
 	}
 	
-	function optionalClearCache(){
+	function optionalClearCache() {
 		$ui.view.tools.dialog({
 			title: $ui.getAjax().T("INFO"),
 			content: data.tsl_installed_module,
@@ -71,12 +70,12 @@ var Store = (function() {
 				name: "SYS_CLEAR_CACHE",
 				style: "primary",
 				callback: function() { $ui.clearCache("dc"); }
-			},{
+			}, {
 				name: "CLOSE",
 				style: "default"
 			}]
 		});
 	}
 	
-    return { fire: fire };
+	return { fire: fire };
 })();
